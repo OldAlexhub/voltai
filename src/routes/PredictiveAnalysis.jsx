84360f8489df0f8c7688103d870487b205a36876
OldAlexhub <info@oldalexhub.com>
2024-09-11 19:36:41 -0400

    updates

--- src/routes/PredictiveAnalysis.jsx
+++ src/routes/PredictiveAnalysis.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 const PredictiveAnalysis = () => {
-  return <div>Comming Soon!</div>;
+  useEffect(() => {
+    const sendUserId = async () => {
+      try {
+        const userId = localStorage.getItem("userId"); // Assuming userId is already stored in localStorage
+
+        if (userId) {
+          const response = await axios.post(
+            `${process.env.REACT_APP_USERID}/${userId}`
+          );
+          console.log("User ID sent successfully:", response.data);
+        } else {
+          console.error("No user ID found in localStorage.");
+        }
+      } catch (error) {
+        console.error("Error sending user ID:", error);
+      }
+    };
+
+    // Call the function when the component mounts
+    sendUserId();
+  }, []); // Empty dependency array ensures this runs only once when the component mounts
+
+  return <div>Coming Soon!</div>;
 };
 
 export default PredictiveAnalysis;
