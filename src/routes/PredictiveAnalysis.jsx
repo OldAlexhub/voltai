4c1315dac0685db1a3a748c5348176149c467435
OldAlexhub <info@oldalexhub.com>
2024-09-12 22:17:37 -0400

    take out R update- will use python

--- src/routes/PredictiveAnalysis.jsx
+++ src/routes/PredictiveAnalysis.jsx
@@ -1,30 +1,7 @@
-import React, { useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
 const PredictiveAnalysis = () => {
-  useEffect(() => {
-    const sendUserId = async () => {
-      try {
-        const userId = localStorage.getItem("userId"); // Assuming userId is already stored in localStorage
-
-        if (userId) {
-          const response = await axios.post(
-            `${process.env.REACT_APP_USERID}/${userId}`
-          );
-          console.log("User ID sent successfully:", response.data);
-        } else {
-          console.error("No user ID found in localStorage.");
-        }
-      } catch (error) {
-        console.error("Error sending user ID:", error);
-      }
-    };
-
-    // Call the function when the component mounts
-    sendUserId();
-  }, []); // Empty dependency array ensures this runs only once when the component mounts
-
-  return <div>Coming Soon!</div>;
+  return <div>Comming Soon</div>;
 };
 
 export default PredictiveAnalysis;
